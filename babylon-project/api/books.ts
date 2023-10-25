08d63be01684c86ef7716758c02de6b4848fe5c5
ThuF <jordan.p.pavlov@gmail.com>
2023-10-25 19:47:04 +0300

    Adds OpenAPI

--- babylon-project/api/books.ts
+++ babylon-project/api/books.ts
@@ -9,9 +9,9 @@ rs.service()
         const entities: Book[] = repository.list();
 
         response.setContentType("application/json");
-        response.setStatus(response.OK);
+        response.setStatus(200);
         response.println(JSON.stringify(entities));
-    })
+    }).produces(["application/json"])
 
     .resource("{id}")
     .get(function (ctx, request, response) {
@@ -20,55 +20,65 @@ rs.service()
 
         response.setContentType("application/json");
         if (entity) {
-            response.setStatus(response.OK);
+            response.setStatus(200);
             response.println(JSON.stringify(entity));
         } else {
-            response.setStatus(response.NOT_FOUND);
+            response.setStatus(404);
             response.println(JSON.stringify({
-                code: response.NOT_FOUND,
+                code: 404,
                 message: "Book not found"
             }));
         }
-    })
+    }).produces(["application/json"])
 
     .resource("/count")
     .get(function (ctx, request, response) {
         const count: number = repository.count();
 
-        response.setStatus(response.OK);
+        response.setStatus(200);
         response.println(`${count}`);
     })
 
     .resource("")
     .post(function (ctx, request, response) {
-        const entity: Book = repository.create(request.getJSON());
+        const entity = request.getJSON();
 
+        entity.id = repository.create(entity);
+
+        response.setContentType("application/json");
+        response.setStatus(201);
         response.setHeader("Content-Location", `/services/ts/babylon-project/service/Books.ts/${entity.id}`);
-        response.setStatus(response.CREATED);
-    })
+        response.println(JSON.stringify(entity));
+    }).produces(["application/json"])
 
     .resource("{id}")
     .put(function (ctx, request, response) {
         const entity = request.getJSON();
         entity.id = ctx.pathParameters.id;
+
         repository.update(entity);
-        response.setStatus(response.OK);
-    })
+
+        response.setContentType("application/json");
+        response.setStatus(200);
+        response.println(JSON.stringify(entity));
+    }).produces(["application/json"])
 
     .resource("{id}")
     .delete(function (ctx, request, response) {
         const id: number = ctx.pathParameters.id;
+
         const entity: Book = repository.findById(id);
 
         if (entity) {
             repository.deleteById(id);
-            response.setStatus(response.NO_CONTENT);
+            response.setStatus(204);
         } else {
-            response.setStatus(response.NOT_FOUND);
+            response.setContentType("application/json");
+            response.setStatus(404);
             response.println(JSON.stringify({
-                code: response.NOT_FOUND,
+                code: 404,
                 message: "Book not found"
             }));
         }
-    })
+    }).produces(["*/*"])
     .execute();
